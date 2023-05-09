ff990e77f371ee713d9daabb533a08e241320e33
danperazzo <daniel.perazzo@impa.br>
2023-05-09 08:03:32 -0300

    [ADDED] Prototype for network

--- train.py
+++ train.py
@@ -12,8 +12,9 @@ import torchvision.transforms as transforms
 import torchvision.datasets as dsets
 from torch.autograd import Variable
 import torch.nn.functional as F
-from fashion_data import fashion
 
+import network
+import dataset
 import wandb
 import os
 
@@ -23,121 +24,47 @@ hyperparameter_defaults = dict(
     channels_two = 32,
     batch_size = 100,
     learning_rate = 0.001,
-    epochs = 2,
+    epochs = 100,
     )
 
-wandb.init(config=hyperparameter_defaults, project="pytorch-cnn-fashion")
+wandb.init(config=hyperparameter_defaults, project="algelin_1rst_attempt")
 config = wandb.config
 
-class CNNModel(nn.Module):
-    def __init__(self):
-        super(CNNModel, self).__init__()
 
-        # Convolution 1
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=config.channels_one, kernel_size=5)
-        self.relu1 = nn.ReLU()
-        # Max pool 1
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
-
-        # Convolution 2
-        self.cnn2 = nn.Conv2d(in_channels=config.channels_one, out_channels=config.channels_two, kernel_size=5, stride=1, padding=0)
-        self.relu2 = nn.ReLU()
-
-        # Max pool 2
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
-
-        self.dropout = nn.Dropout(p=config.dropout)
-
-        # Fully connected 1 (readout)
-        self.fc1 = nn.Linear(config.channels_two*4*4, 10)
-
-    def forward(self, x):
-        # Convolution 1
-        out = self.cnn1(x)
-        out = self.relu1(out)
-
-        # Max pool 1
-        out = self.maxpool1(out)
-
-        # Convolution 2
-        out = self.cnn2(out)
-        out = self.relu2(out)
-
-        # Max pool 2
-        out = self.maxpool2(out)
-
-        # Resize
-        # Original size: (100, 32, 7, 7)
-        # out.size(0): 100
-        # New out size: (100, 32*7*7)
-        out = out.view(out.size(0), -1)
-        out = self.dropout(out)
-        # Linear function (readout)
-        out = self.fc1(out)
-
-        return out
 
 def main():
-    normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
-                                         std=[x/255.0 for x in [63.0, 62.1, 66.7]])
+    
+    model = network.Siren(in_features=2, out_features=1, first_omega_0=30, 
+                            hidden_omega_0= 30, hidden_features=256, hidden_layers=3, outermost_linear=True)
 
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize((0.1307,), (0.3081,))])
+    train_signal_dataset = dataset.Func1DWrapper(range=(-0.5, 0.5),
+                                         fn=fn,
+                                         sampling_density=1000,
+                                         train_every=1000/18)
 
-    train_dataset = fashion(root='./data',
-                                train=True,
-                                transform=transform,
-                                download=True
-                               )
 
-    test_dataset = fashion(root='./data',
-                                train=False,
-                                transform=transform,
-                               )
-
-    label_names = [
-        "T-shirt or top",
-        "Trouser",
-        "Pullover",
-        "Dress",
-        "Coat",
-        "Sandal",
-        "Shirt",
-        "Sneaker",
-        "Bag",
-        "Boot"]
-
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=config.batch_size,
+    train_loader = torch.utils.data.DataLoader(dataset=train_signal_dataset,
+                                               batch_size=1,
                                                shuffle=True)
 
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                              batch_size=config.batch_size,
-                                              shuffle=False)
-
-
-    model = CNNModel()
     wandb.watch(model)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
 
-    iter = 0
     for epoch in range(config.epochs):
-        for i, (images, labels) in enumerate(train_loader):
-
-            images = Variable(images)
-            labels = Variable(labels)
+        
+        for i, (coords, values) in enumerate(train_loader):
 
             # Clear gradients w.r.t. parameters
             optimizer.zero_grad()
 
             # Forward pass to get output/logits
-            outputs = model(images)
+            outputs = model(coords)
 
             # Calculate Loss: softmax --> cross entropy loss
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, values)
 
             # Getting gradients w.r.t. parameters
             loss.backward()
@@ -145,43 +72,19 @@ def main():
             # Updating parameters
             optimizer.step()
 
-            iter += 1
 
-            if iter % 100 == 0:
-                # Calculate Accuracy
-                correct = 0.0
-                correct_arr = [0.0] * 10
-                total = 0.0
-                total_arr = [0.0] * 10
+        if epoch % 10 == 0:
+            with torch.no_grad():
+                # Forward pass to get output/logits
+                outputs = model(coords)
 
-                # Iterate through test dataset
-                for images, labels in test_loader:
-                    images = Variable(images)
-
-                    # Forward pass only to get logits/output
-                    outputs = model(images)
-
-                    # Get predictions from the maximum value
-                    _, predicted = torch.max(outputs.data, 1)
-
-                    # Total number of labels
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum()
-
-                    for label in range(10):
-                        correct_arr[label] += (((predicted == labels) & (labels==label)).sum())
-                        total_arr[label] += (labels == label).sum()
-
-                accuracy = correct / total
-
-                metrics = {'accuracy': accuracy, 'loss': loss}
-                for label in range(10):
-                    metrics['Accuracy ' + label_names[label]] = correct_arr[label] / total_arr[label]
-
-                wandb.log(metrics)
-
-                # Print Loss
-                print('Iteration: {0} Loss: {1:.2f} Accuracy: {2:.2f}'.format(iter, loss, accuracy))
+                # Calculate Loss: softmax --> cross entropy loss
+                loss = criterion(outputs, values)
+            
+            metrics = {'loss': loss}
+            wandb.log(metrics)
+            # Print Loss
+            print('Iteration: {0} Loss: {1:.2f} '.format(iter, loss))
     torch.save(model.state_dict(), os.path.join(wandb.run.dir, "model.pt"))
 
 if __name__ == '__main__':
