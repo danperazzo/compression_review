ff6cb6358d04a523188ac13ab053c8750fb79966
danperazzo <drp@cin.ufpe.br>
2023-06-05 13:37:46 -0300

    [ADDED] loop to run tests with different metrics

--- test.py
+++ test.py
@@ -1,25 +1,59 @@
 import os
 
 from compression_transforms import compress_rgb, all_methods
-from utils import imwrite, imread, prepare_results_folder
+from utils import imwrite, imread, prepare_results_folder, PSNR, ssim, lpips, average_l
+import pandas as pd
+import time
+
 
 order = 1
-method = 'kl'
+method = 'fourrier'
 block_shape = (8,8)
 data_folder = 'datasets/kodak/'
 
 results_folder = prepare_results_folder(order, block_shape, all_methods)
 
 img_list = os.listdir( data_folder )
+ssim_list = []
+psnr_list = []
+lpips_list = []
+time_elapsed = []
 
 for imname in img_list:
 
-	img_rgb = imread(data_folder, imname) 
+	img_rgb = imread(data_folder, imname)
+	start = time.time()
 	img_rgb_compress = compress_rgb(img_rgb, order,block_shape, compression = method)
+	end = time.time()
 	imwrite(results_folder , method ,imname,img_rgb_compress)
+
+	ssim_val = ssim(img_rgb,img_rgb_compress)
+	psnr_val = PSNR(img_rgb,img_rgb_compress)
+	lpips_val = lpips(img_rgb, img_rgb_compress)
+	time_val = end - start
+
+
+	ssim_list.append(ssim_val)
+	psnr_list.append(psnr_val)
+	lpips_list.append(lpips_val)
+	time_elapsed.append(time_val)
 
 	print(imname)
 
+ssim_avg = average_l(ssim_list)
+psnr_avg = average_l(psnr_list)
+lpips_avg = average_l(lpips_list)
+time_avg = average_l(time_elapsed)
 
-print(img_rgb.shape)
+
+df = pd.DataFrame({'image_name': (img_list+['mean']),
+                   'ssim': ssim_list + [ssim_avg],
+                   'psnr': psnr_list + [psnr_avg],
+				   'lpips': lpips_list + [lpips_avg],
+				   'time':time_elapsed + [time_avg]})
+
+csv_path = results_folder + '/' + method +'/results_num.csv'
+df.to_csv(csv_path)  
+
+print(csv_path)
 
---
+++ utils.py
@@ -0,0 +1,100 @@
+import cv2
+from PIL import Image
+import numpy as np
+import os
+import scipy
+import torch
+
+def average_l(lst):
+    return sum(lst) / len(lst)
+
+def rgb_ssim(img0, img1, max_val,
+             filter_size=11,
+             filter_sigma=1.5,
+             k1=0.01,
+             k2=0.03,
+             return_map=False):
+    # Modified from https://github.com/google/mipnerf/blob/16e73dfdb52044dcceb47cda5243a686391a6e0f/internal/math.py#L58
+    assert len(img0.shape) == 3
+    assert img0.shape[-1] == 3
+    assert img0.shape == img1.shape
+
+    # Construct a 1D Gaussian blur filter.
+    hw = filter_size // 2
+    shift = (2 * hw - filter_size + 1) / 2
+    f_i = ((np.arange(filter_size) - hw + shift) / filter_sigma)**2
+    filt = np.exp(-0.5 * f_i)
+    filt /= np.sum(filt)
+
+    # Blur in x and y (faster than the 2D convolution).
+    def convolve2d(z, f):
+        return scipy.signal.convolve2d(z, f, mode='valid')
+
+    filt_fn = lambda z: np.stack([
+        convolve2d(convolve2d(z[...,i], filt[:, None]), filt[None, :])
+        for i in range(z.shape[-1])], -1)
+    mu0 = filt_fn(img0)
+    mu1 = filt_fn(img1)
+    mu00 = mu0 * mu0
+    mu11 = mu1 * mu1
+    mu01 = mu0 * mu1
+    sigma00 = filt_fn(img0**2) - mu00
+    sigma11 = filt_fn(img1**2) - mu11
+    sigma01 = filt_fn(img0 * img1) - mu01
+
+    # Clip the variances and covariances to valid values.
+    # Variance must be non-negative:
+    sigma00 = np.maximum(0., sigma00)
+    sigma11 = np.maximum(0., sigma11)
+    sigma01 = np.sign(sigma01) * np.minimum(
+        np.sqrt(sigma00 * sigma11), np.abs(sigma01))
+    c1 = (k1 * max_val)**2
+    c2 = (k2 * max_val)**2
+    numer = (2 * mu01 + c1) * (2 * sigma01 + c2)
+    denom = (mu00 + mu11 + c1) * (sigma00 + sigma11 + c2)
+    ssim_map = numer / denom
+    ssim = np.mean(ssim_map)
+    return ssim_map if return_map else ssim
+
+
+__LPIPS__ = {}
+def init_lpips(net_name, device):
+    assert net_name in ['alex', 'vgg']
+    import lpips
+    print(f'init_lpips: lpips_{net_name}')
+    return lpips.LPIPS(net=net_name, version='0.1').eval().to(device)
+
+def rgb_lpips(np_gt, np_im, net_name, device):
+    if net_name not in __LPIPS__:
+        __LPIPS__[net_name] = init_lpips(net_name, device)
+    gt = torch.from_numpy(np_gt).permute([2, 0, 1]).contiguous().to(device)
+    im = torch.from_numpy(np_im).permute([2, 0, 1]).contiguous().to(device)
+    return __LPIPS__[net_name](gt, im, normalize=True).item()
+
+
+PSNR = lambda x,y : -10. * np.log10(np.mean(np.square(x/255 - y/255)))
+ssim = lambda x,y :rgb_ssim(x/255, y/255, max_val=1)
+lpips = lambda x,y :rgb_lpips(np.float32(x/255), np.float32(y/255), net_name='vgg', device=torch.device("cpu"))
+
+
+def imwrite(results_folder,method, name ,img):
+    cv2.imwrite(results_folder + '/' + method + '/' +name,img)
+
+def imread(data_folder, name):
+    img_rgb = cv2.imread(data_folder + '/' + name)
+    return img_rgb
+
+def prepare_results_folder(order, block_shape, all_methods):
+    results_folder = 'results_' + f'order_{order}_'+f'blocks_{block_shape}'
+    
+    if not os.path.exists(results_folder):
+        os.mkdir(results_folder)
+
+        for method in all_methods:
+            method_folder = results_folder+'/'+method
+            if not os.path.exists(method_folder):
+                os.mkdir(method_folder)
+
+    return results_folder
+
+
