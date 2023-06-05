ba3d6d4a4ee72e65d1fb37ce16b1127650eb3265
danperazzo <drp@cin.ufpe.br>
2023-06-05 15:57:01 -0300

    [ADDED] set-up to do multiple tests

--- test.py
+++ test.py
@@ -6,54 +6,61 @@ import pandas as pd
 import time
 
 
-order = 4
-method = 'kl'
-block_shape = (8,8)
+orders = [0.20,0.40,0.60,0.80]
+methods = ['kl', 'fourrier', 'dct','svd']
+block_shapes = [(8,8), (16,16), (32,32), (64,64)]
 data_folder = 'datasets/kodak/'
 
-results_folder = prepare_results_folder(order, block_shape, all_methods)
 
-img_list = os.listdir( data_folder )
-ssim_list = []
-psnr_list = []
-lpips_list = []
-time_elapsed = []
+def test_for_parameters(order, method, block_shape, data_folder):
 
-for imname in img_list:
+	results_folder = prepare_results_folder(order, block_shape, all_methods)
 
-	img_rgb = imread(data_folder, imname)
-	start = time.time()
-	img_rgb_compress = compress_rgb(img_rgb, order,block_shape, compression = method)
-	end = time.time()
-	imwrite(results_folder , method ,imname,img_rgb_compress)
+	img_list = os.listdir( data_folder )
+	ssim_list = []
+	psnr_list = []
+	lpips_list = []
+	time_elapsed = []
 
-	ssim_val = ssim(img_rgb,img_rgb_compress)
-	psnr_val = PSNR(img_rgb,img_rgb_compress)
-	lpips_val = lpips(img_rgb, img_rgb_compress)
-	time_val = end - start
+	for imname in img_list:
 
+		img_rgb = imread(data_folder, imname)
+		start = time.time()
+		img_rgb_compress = compress_rgb(img_rgb, order,block_shape, compression = method)
+		end = time.time()
+		imwrite(results_folder , method ,imname,img_rgb_compress)
 
-	ssim_list.append(ssim_val)
-	psnr_list.append(psnr_val)
-	lpips_list.append(lpips_val)
-	time_elapsed.append(time_val)
-
-	print(imname)
-
-ssim_avg = average_l(ssim_list)
-psnr_avg = average_l(psnr_list)
-lpips_avg = average_l(lpips_list)
-time_avg = average_l(time_elapsed)
+		ssim_val = ssim(img_rgb,img_rgb_compress)
+		psnr_val = PSNR(img_rgb,img_rgb_compress)
+		lpips_val = lpips(img_rgb, img_rgb_compress)
+		time_val = end - start
 
 
-df = pd.DataFrame({'image_name': (img_list+['mean']),
-                   'ssim': ssim_list + [ssim_avg],
-                   'psnr': psnr_list + [psnr_avg],
-				   'lpips': lpips_list + [lpips_avg],
-				   'time':time_elapsed + [time_avg]})
+		ssim_list.append(ssim_val)
+		psnr_list.append(psnr_val)
+		lpips_list.append(lpips_val)
+		time_elapsed.append(time_val)
 
-csv_path = results_folder + '/' + method +'/results_num.csv'
-df.to_csv(csv_path)  
+		print(imname)
 
-print(csv_path)
+	ssim_avg = average_l(ssim_list)
+	psnr_avg = average_l(psnr_list)
+	lpips_avg = average_l(lpips_list)
+	time_avg = average_l(time_elapsed)
+
+
+	df = pd.DataFrame({'image_name': (img_list+['mean']),
+					'ssim': ssim_list + [ssim_avg],
+					'psnr': psnr_list + [psnr_avg],
+					'lpips': lpips_list + [lpips_avg],
+					'time':time_elapsed + [time_avg]})
+
+	csv_path = results_folder + '/' + method +'/results_num.csv'
+	df.to_csv(csv_path)  
+
+for block_shape in block_shapes:
+	for order in orders:
+		order = int(order*block_shape[0])
+		for method in methods:
+			test_for_parameters(order, method, block_shape, data_folder)
 
